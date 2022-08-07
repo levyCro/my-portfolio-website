f0d6667ad3b9e33a08671f6d91f3a96fe47a34d0
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-08-07 10:03:12 +0200

    adding a dynamic navbar with hamburger menu when smaller screens

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Profile from './Profile/Profile'
-import Footer from './Footer/Footer'
-import './Home.css'
+import React from "react";
+import Header from "./Header/Header";
+import Profile from "./Profile/Profile";
+import Footer from "./Footer/Footer";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home-container">
-        <Profile /> 
-         <Footer />
-
+      <Header />
+      <Profile />
+      <Footer />
     </div>
-  )
+  );
 }
